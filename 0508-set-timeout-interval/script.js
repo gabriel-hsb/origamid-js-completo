134893fe0a0fbd6f57d5ce98fb52fe6f3e7562cd
Gabriel <ghbranco6@gmail.com>
2024-05-22 17:59:57 -0300

    remote

---
+++ 0508-set-timeout-interval/script.js
@@ -0,0 +1,79 @@
+// https://www.origamid.com/slide/javascript-completo-es6/#/0508-settimeout-e-setinterval/10
+
+// NÃO USAR ASSIM
+function espera(texto) {
+  console.log(texto);
+}
+// setTimeout(espera, 1000, "Depois de 1s");
+
+//  USAR ASSIM
+
+setTimeout(() => {
+  // console.log("passou 2s");
+}, 2000);
+
+// const contarAte10 = setInterval(callback, 1000);
+
+// let i = 0;
+// function callback() {
+//   // console.log(i++);
+//   if (i > 10) {
+//     clearInterval(contarAte10);
+//   }
+// }
+
+// Mude a cor da tela para azul e depois para vermelho a cada 2s.
+
+const bodyStyle = document.body.style;
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+for (let i = 0; i < 20; i++) {
+  setTimeout(() => {
+    isEven(i)
+      ? (bodyStyle.background = "blue")
+      : (bodyStyle.background = "red");
+  }, 2000 * i);
+}
+
+// -------------------------
+
+// Crie um cronometro utilizando o setInterval. Deve ser possível
+// iniciar, pausar e resetar (duplo clique no pausar).
+
+const btnStart = document.querySelector('[data-btn="start"]');
+const btnStop = document.querySelector('[data-btn="stop"]');
+
+btnStop.addEventListener("click", stopTimer);
+btnStop.addEventListener("dblclick", resetTimer);
+
+btnStart.addEventListener("click", startTimer);
+
+function stopTimer() {
+  btnStop.removeEventListener("click", stopTimer);
+
+  const foo = setInterval(() => {
+    btnStop.addEventListener("click", stopTimer);
+    clearInterval(timer);
+
+    clearInterval(foo);
+  }, 400);
+}
+
+function resetTimer() {
+  i = 0;
+  console.log("clicou double");
+}
+
+let runTimer = false;
+
+function startTimer() {
+  runTimer = true;
+}
+
+let i = 0;
+const timer = setInterval(() => {
+  if (runTimer) console.log(i++);
+}, 1000);
